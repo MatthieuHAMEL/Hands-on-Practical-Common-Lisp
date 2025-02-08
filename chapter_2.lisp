7f0a401248af5a727597c4379ee499e1d5331450
HAMEL <75175897+MatthieuHAMEL@users.noreply.github.com>
2025-02-08 16:33:27 +0100

    beginning...

---
+++ chapter_2.lisp
@@ -0,0 +1,14 @@
+;;;; Chapter 2 : A tour of the REPL
+;;;; I took the "Portacle" emacs distribution, since "Lisp in a Box"
+;;;; was a bit outdated.
+
+;;; read-eval-print loop (REPL)
+
+; (+ 2 3) ; List of 3 elements : +, 2 and 3. + is evaluated as a function
+;-> 5
+
+; With a string : 
+; > "hello, world"
+; "hello, world"
+
+
